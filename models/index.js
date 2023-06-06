95fbc8a153dd1dda627cbe20c84ac593385f1fb2
apichet06 <apichet06@gmail.com>
2023-06-06 16:05:12 +0700

    3rd commit

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
+require('dotenv').config();
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
@@ -14,7 +14,11 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+  sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
+    host: process.env.host,
+    dialect: 'mysql',
+    logging: false,
+  });
   // sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
