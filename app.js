6199a51806e05fc2d084d3575e38c09c7249173e
apichet06 <apichet06@gmail.com>
2023-06-08 08:46:15 +0700

    8rd commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors');
-
+require('dotenv').config();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
--- models/index.js
+++ models/index.js
@@ -7,24 +7,38 @@ const Sequelize = require('sequelize');
 
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
-    host: process.env.host,
-    dialect: 'mysql',
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
     dialectOptions: {
       ssl: {
-        rejectUnauthorized: true,
+        require: true,
+        rejectUnauthorized: true
       }
     }
   });
-  // sequelize = new Sequelize(config.database, config.username, config.password, config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true
+      }
+    }
+  });
 }
 
 fs
@@ -50,5 +64,4 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
